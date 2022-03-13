7f1da0c0c79338e92f641cf50ea3bd584e83b536
Taylor L <tlandich@umich.edu>
2022-03-13 04:04:33 -0400

    Save+Load working
    Need to add more editor tiles, get prefab scripts working again, remake levels, add a way to put in prefabs into the editor (for later)

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,25 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T : Component
-{
+//  Code from:
+// https://www.youtube.com/watch?v=ErJgQY5smnw&t=3s
+
+public abstract class Singleton<T> : MonoBehaviour where T : Component {
     private static T instance;
 
     protected static bool DontDestroy = true;
 
     private static bool m_applicationIsQuitting = false;
 
-    public static T GetInstance()
-    {
+    public static T GetInstance () {
         if (m_applicationIsQuitting) { return null; }
 
-        if (instance == null)
-        {
-            instance = FindObjectOfType<T>();
-            if (instance == null)
-            {
-                GameObject obj = new GameObject();
-                obj.name = typeof(T).Name;
-                instance = obj.AddComponent<T>();
+        if (instance == null) {
+            instance = FindObjectOfType<T> ();
+            if (instance == null) {
+                GameObject obj = new GameObject ();
+                obj.name = typeof (T).Name;
+                instance = obj.AddComponent<T> ();
             }
         }
         return instance;
@@ -33,22 +32,16 @@ public abstract class Singleton<T> : MonoBehaviour where T : Component
      * }
      * */
 
-    protected virtual void Awake()
-    {
-        if (instance == null)
-        {
+    protected virtual void Awake () {
+        if (instance == null) {
             instance = this as T;
-            if (DontDestroy) DontDestroyOnLoad(gameObject);
-        }
-        else if (instance != this as T)
-        {
-            Destroy(gameObject);
-        }
-        else if (DontDestroy) { DontDestroyOnLoad(gameObject); }
+            if (DontDestroy) DontDestroyOnLoad (gameObject);
+        } else if (instance != this as T) {
+            Destroy (gameObject);
+        } else if (DontDestroy) { DontDestroyOnLoad (gameObject); }
     }
 
-    private void OnApplicationQuit()
-    {
+    private void OnApplicationQuit () {
         m_applicationIsQuitting = true;
     }
 }
--- Assets/Scripts/unlock.cs
+++ Assets/Scripts/unlock.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class unlock : MonoBehaviour
+public class Unlock : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision) 
     {
@@ -13,4 +13,5 @@ public class unlock : MonoBehaviour
             Destroy(gameObject);
         }   
     }
+
 }
